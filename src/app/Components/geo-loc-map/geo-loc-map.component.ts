9fd60bac37bfde28b9e75d6bb7a3f86e95ce1302
sean_kelly <sean@kelly3d.com>
2022-01-07 00:22:29 +0000

    world map creation

--- src/app/Components/geo-loc-map/geo-loc-map.component.ts
+++ src/app/Components/geo-loc-map/geo-loc-map.component.ts
@@ -1,6 +1,6 @@
-import { Component, AfterViewInit, HostListener, OnInit } from '@angular/core';
+import { Component, AfterViewInit, HostListener, OnInit, OnChanges } from '@angular/core';
 import * as d3 from 'd3';
-import * as t from 'topojson';
+import * as feature from 'topojson';
 import { Countries } from './countries';
 
 @Component({
@@ -8,7 +8,7 @@ import { Countries } from './countries';
   templateUrl: './geo-loc-map.component.html',
   styleUrls: ['./geo-loc-map.component.scss']
 })
-export class GeoLocMapComponent implements AfterViewInit {
+export class GeoLocMapComponent implements OnInit, OnChanges {
   name = 'd3';
   mapFeatures: any;
   airports: any;
@@ -19,77 +19,17 @@ export class GeoLocMapComponent implements AfterViewInit {
   event!: MouseEvent;
 
   svg: any;
+  paths: any;
   g: any;
 
-  public ngAfterViewInit() {
+  public ngOnInit() {
     this.createMap();
   }
 
-  getZoomValue(event: MouseEvent): void {
+  public ngOnChanges() {
+    // this.paths.call(this.zoom);
 
-    d3.json('../../../assets/world-110m2.json').then( (topology: any) => {
-
-    })
-
-    let wrapper = document.getElementById('wrapper');
-    console.log(wrapper)
-
-    const regexMatch = /[0-9]/g;
-    const path = event.composedPath()[3] as HTMLInputElement;
-    const worldMapPath = path.childNodes[0].childNodes[0] as SVGAElement;
-    const s = new XMLSerializer();
-    let str: string = '';
-    if(worldMapPath !== null) {
-      str = s.serializeToString(worldMapPath);
-      
-      if(str !== null) {
-
-        let el4 = str.split(' ')[4];
-
-        // if( regexMatch != null ) {
-        //   let match = regexMatch;
-        //   el4.match(match).toString().replace(',', '.').replace(/\,/g, '');
-        // }
-      }
-    }
-    if (str === '1.7120') { str = '1'; }
-    const inputPosition = parseFloat((document.getElementById('zoomer') as HTMLInputElement).value);
-    this.zoomValue = parseFloat(str) + inputPosition;
-    if (this.zoomValue > 5) { this.zoomValue = 5; }
-    const zoomValeStr = this.zoomValue.toString();
-    (document.getElementById('zoomer') as HTMLInputElement).value = zoomValeStr;
-    this.setZoom(this.zoomValue);
-
-    return this.zoomValue;
   }
-
-  setZoom(zoomValue: number): void {
-    zoomValue = Math.round(this.zoomValue);
-    let worldMap = (document.getElementById('worldMap')) as HTMLElement;
-    worldMap = (`transform: scale(${(document.getElementById('zoomer') as HTMLInputElement).value})`) as unknown as HTMLElement ;
-  }
-
-  @HostListener('dblclick', ['$event'])
-  onMouseZoom(event: MouseEvent): void {
-    let count = 0;
-    if (event) { count++; }
-    if (count > 0) {
-      let wrapper = document.getElementById('wrapper') as HTMLElement;
-      wrapper.scrollTo(event.clientX - screenLeft, event.clientY / 2 - screenTop);
-    }
-    if ((event.composedPath()[3] as HTMLElement).id === 'worldMap') {
-
-      this.getZoomValue(event);
-    }
-  }
-
-  @HostListener('drag', ['$event'])
-  whileDrag(event: MouseEvent): void {
-    const xCursor = event.clientX;
-    const yCursor = event.clientY;
-    const imgEle = document.getElementById('worldContainer') as HTMLElement;
-  }
-
 
   createMap() {
     console.log('running')
@@ -101,94 +41,50 @@ export class GeoLocMapComponent implements AfterViewInit {
       const width = 620;
       const height = 396;
 
+      this.svg = d3.select('#wrapper');
+      
       const projection = d3.geoMercator()
         .translate([ width / 2, height / 2])
         .scale(100);
 
-      const svg = d3.select('#worldMap').append('svg')
-        .attr('id', 'container')
-        .attr('width', width)
-        .attr('height', height);
-      const path = d3.geoPath()
-        .projection(projection);
-      const g = svg.append('g');
-      g.attr('class', 'map');
-
-      d3.json('../../../assets/world-110m2.json')
+      d3.json('../../assets/world.topojson')
       .then((topology: any) => {
-        this.mapFeatures = t.feature(topology, topology.objects.countries);
-        for (let i = 0; i < 177; i++) {
-          this.countries.push(topology.objects.countries.geometries[i].id);
-        }
+        
+        let pathGenerator = d3.geoPath().projection(projection);
+        const { countries } = topology.objects;
+        this.mapFeatures = feature.feature(topology, countries);
+        let countriesGeometry = this.mapFeatures.features;
 
-        g.selectAll('path')
-        .data(this.mapFeatures.features)
+        this.paths = d3.select('#wrapper')
+        .append('svg')
+        .attr('width', '100%')
+        .attr('height', '396px')
+        .attr('position', 'relative')
+          .selectAll('path')
+          .data(countriesGeometry)
         .enter()
         .append('path')
-        .attr('class', 'countries')
-          .attr('stroke', '#171c20')
-          .attr('cursor', 'pointer')
-          .attr('stroke-width', '0.1')
-          .attr('fill', 'white')
-          // .attr('d', path)
-          // .on('click', function(j: any, d: any, k) {
-
-          //   g.selectAll('path').style('fill', 'white');
-          //   const mousePos = d3.mouse(this);
-          //   d3.select(this).style('fill', 'red');
-          //   const posX = Math.floor(mousePos[0]);
-          //   const posY = Math.floor(mousePos[1]);
-
-          //   let capitals = '';
-          //   let locations = '';
-
-          //   for (let i = 0; i < country2.length; i++) {
-          //     if ( j.id === country2[i].id ) {
-          //       for (let k = 0; k < countryCities[i].cities.length; k++) {
-          //         locations += `<div>${countryCities[i].cities[k].city}</div>`;
-          //       }
-          //     }
-          //   }
-
-          //   d3.select('.selectedCountry')
-          //   .text(country)
-
-          //   .append('div')
-          //   .attr('class', 'countryModal')
-          //   .attr('width', 400)
-          //   .attr('height', 400)
-          //   .attr('z-index', 35)
-          //   // .attr('top', 400)
-          //   .attr('position', 'absolute')
-          //   .style('background-color', 'blue')
-          //   .html( (i: any) => {
-          //     capitals = locations;
-          //     return capitals;
-          //   });
-
-          // })
-          .on('mouseover', function(d: any) {
-            for (const nation of country2) {
-              if (d.id === nation.id) {
-                country = nation.name;
-                break;
-              }
-            }
-            // const mousePos = d3.mouse(this);
-            // const posX = Math.floor(mousePos[0]);
-            // const posY = Math.floor(mousePos[1]);
-            d3.select('.countryNames')
-            // .style('transform', `translate( ${posX + 8}px, ${posY + 10}px )`)
-            .style('background-color', '#33333388')
-            .style('padding', '4px')
-            .style('padding-left', '8px')
-            .style('padding-right', '8px')
-            .style('border-radius', '2px')
-            .style('display', 'block')
-            .text(country);
-
-          });
+        .attr('class', 'country')
+        .attr('d', ( d: any) => pathGenerator(d))
+        .attr('stroke', '#171c20')
+        .attr('cursor', 'pointer')
+        .attr('stroke-width', '0.1')
+        .attr('fill', 'white')
+        .on('mouseover', (d) => {
+          d.target.style.fill = 'red';
+        })
+        .on('mouseout', (d) => {
+          d.target.style.fill = 'white';
+        })
+        this.paths.call(this.zoom);
       });
   }
 
+  zoom = d3.zoom()
+    .scaleExtent([1, 40])
+    .on('zoom', (event) => {
+      console.log(event);
+      
+      this.paths.selectAll('g').attr('transform', event.transform.k)
+    });
 }
