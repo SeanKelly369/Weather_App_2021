1c0a04e659e47810f9f394ff4c0df7d5808c45c7
sean_kelly <sean@kelly3d.com>
2022-01-09 00:15:47 +0000

    country's submenu added

--- src/app/Components/geo-loc-map/geo-loc-map.component.ts
+++ src/app/Components/geo-loc-map/geo-loc-map.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import * as d3 from 'd3';
-import { selectAll } from 'd3';
 import * as feature from 'topojson';
 import { Countries } from './countries';
 
@@ -10,14 +9,17 @@ import { Countries } from './countries';
   styleUrls: ['./geo-loc-map.component.scss']
 })
 export class GeoLocMapComponent implements OnInit, OnChanges {
-  name = 'd3';
+  name: string = 'd3';
   mapFeatures: any;
   airports: any;
   nodeSelection: any;
-  countries: any[] = new Countries().countries;
+  countries: Object[] = new Countries().countries;
+  countryCities: Object[] = new Countries().countryCities;
   scaledInitial = 1;
   zoomValue: any = 1;
   event!: MouseEvent;
+  selectedCountry: number = -1;
+  selectedCountyCities: Object = [];
 
   svg: any;
   paths: any;
@@ -25,6 +27,7 @@ export class GeoLocMapComponent implements OnInit, OnChanges {
   g: any;
   width: number = 620;
   height: number = 400;
+  countryCoords: any = [];
 
   public ngOnInit() {
     this.createMap();
@@ -37,10 +40,10 @@ export class GeoLocMapComponent implements OnInit, OnChanges {
 
   createMap() {
 
-      const country2: any[] = new Countries().countries;
-      const countryCities: any[] = new Countries().countryCities;
+      // const countries: any[] = new Countries().countries;
+      // const countryCities: any[] = new Countries().countryCities;
 
-      let country = '';
+      let coords: any;
       
       const projection = d3.geoMercator()
         .translate([ this.width / 2, this.height / 2])
@@ -53,9 +56,10 @@ export class GeoLocMapComponent implements OnInit, OnChanges {
         this.mapFeatures = feature.feature(topology, countries);
         const countriesGeometry = this.mapFeatures.features;
 
-        this.svg = d3.select('#wrapper').attr('transformX', this.width/2)
-        .attr('right', '300px');
+        this.svg = d3.select('#wrapper')
 
+        
+        const dynamicWidth = (document.getElementById('mapContainer')?.clientWidth as number) / 4;
         this.svg
         .append('svg')
         .attr('width', '100%')
@@ -63,8 +67,7 @@ export class GeoLocMapComponent implements OnInit, OnChanges {
         .attr('position', 'relative')
         .append('g')
         .attr('position', 'relative')
-        // .attr('x', '-200')
-        .attr('transform', `translate(${this.width / 2}, 0)`)
+        .attr('transform', 'translate(' + dynamicWidth + ')' )
         .selectAll('path')
         .data(countriesGeometry)
         .enter()
@@ -78,15 +81,66 @@ export class GeoLocMapComponent implements OnInit, OnChanges {
         .attr('stroke-width', '0.1')
         .attr('fill', 'white')
         .on('mouseover', (d: any) => {
-          d.target.style.fill = '#f1f1f1';
+          d.target.style.fill = '#e1e1e1';
+
         })
         .on('mouseout', (d: any) => {
           d.target.style.fill = 'white';
         })
-        .on('click', (d: any) => {
-          console.log(d);
-          console.log(d.target.__data__.properties.name);
-          console.log(d.target.__data__.id);
+        .on('click', (event: any, country: any ) => {
+          console.log(country)
+          event.target.style.fill = 'red'
+
+          this.countryCities.forEach( ( (d:any) => {
+            console.log(d.id);
+            if(d.id === country.id) {
+              coords = d;
+            }
+
+          }));
+          d3.select('#countryDetails')
+          .selectChildren()
+          .remove();
+          d3.select('#countryDetails')
+          .append('div')  
+          .attr('top', 400)
+          .text(country.properties.name)
+          .attr('position', 'absolute')
+          .attr('z-index', 3000)
+
+          d3.select('#countryDetails')
+          .append('hr')
+          .attr('background-color', 'white')
+          .attr('color', 'white')
+          .attr('width', '100%')
+          .attr('height', '1px');
+
+          this.countryCoords.length = 0;
+
+          coords.cities.forEach( (element: any) => {
+            console.log(element);
+            this.countryCoords.push( element );
+            d3.select('#countryDetails')
+            .append('div')
+            .style('font-size', '.8rem')
+            .style('margin-bottom', '.3rem')
+            .style('cursor', 'pointer')
+            .text(element.city)
+            .on('click', ((e) => {
+              let countryLocation = e.srcElement.innerText;
+
+              console.log(this.countryCoords);
+              this.countryCoords.forEach( (element: any) => {
+                if(countryLocation === element.city) {
+                  console.log('match');
+                  let queryCoordinates = element.coords;
+                  console.log(queryCoordinates);
+                }
+              });
+            }))
+            
+          });
+          console.log(this.countryCoords);
         });
 
         this.svg.call(this.zoom);
@@ -94,7 +148,7 @@ export class GeoLocMapComponent implements OnInit, OnChanges {
   }
 
   zoom = d3.zoom()
-    .scaleExtent([1, 40])
+    .scaleExtent([1.1, 40])
     .translateExtent([[0, -80],[this.width, this.height]])
     .on('zoom', (event) => {
 
